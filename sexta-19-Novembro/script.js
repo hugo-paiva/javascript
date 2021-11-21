38f035794c86e533af6baedf3be523d8689541df
Hugo Paiva <hugosousa76@gmail.com>
2021-11-21 01:04:05 -0300

    adding imagem using loop for appendChild

--- sexta-19-Novembro/script.js
+++ sexta-19-Novembro/script.js
@@ -3,11 +3,25 @@ p.innerHTML = 'Bom dia!'
 
 document.querySelector('#conteudo').appendChild(p)
 
-let img = document.createElement('img')
 
-img.src = 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*'
+let albumList = [
+    "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/in-through-the-out-door-1555362294.jpg?crop=1xw:1xh;center,top&resize=640:*",
+    "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/presence-led-zeppelin-1555363010.jpg?crop=0.9971617786187322xw:1xh;center,top&resize=640:*",
+    "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/physical-graffiti-1555363262.jpg?crop=1xw:1xh;center,top&resize=640:*",
+    "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/led-zeppelin-1-1555363346.jpg?crop=1xw:1xh;center,top&resize=640:*",
+    "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/led-zeppelin-ii-1555363460.jpg?crop=1xw:1xh;center,top&resize=640:*",
+    "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/led-zeppelin-iv-1555363513.jpg?crop=1xw:1xh;center,top&resize=640:*",
+    "https://lh3.googleusercontent.com/-FGILtwdtfVI/TXFDexd2wyI/AAAAAAAABOI/Hx9bALbsdoU/s1600/Pink_Floyd_-_The_Division_Bell.jpg"
+]
 
-document.querySelector('#conteudo').appendChild(img)
+let amountOfImages = albumList.length 
 
-img.style.width = '300px'
+for (let i = 0; i < amountOfImages; i++) {
+    var img = document.createElement('img')
+    img.src = albumList.pop()
+    document.querySelector('#conteudo').appendChild(img)
+    img.style.width = '300px'
+    img.style.padding = '1rem'
+}
+
 
