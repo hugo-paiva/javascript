6e12c4d1f034608cc454f03c3f34b0b21f074558
Hugo Paiva <hugosousa76@gmail.com>
2021-11-24 01:20:07 -0300

    acupunture and stakehouse

---
+++ terca-23-nov/stylescript.js
@@ -0,0 +1,13 @@
+//Styles
+let main = document.querySelector('main')
+main.style.width = '60%'
+main.style.marginInline = 'auto'
+
+let opinion = document.querySelector('#opinion')
+opinion.style.width = '35rem'
+opinion.style.height = '6rem'
+
+let advice = document.querySelector('#advices')
+advice.style.width = '35rem'
+advice.style.height = '6rem'
+
