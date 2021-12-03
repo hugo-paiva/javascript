617039fee813658c520d60c566eac0663b0eff1a
Hugo Paiva <hugosousa76@gmail.com>
2021-12-03 04:51:41 -0300

    comments added some and reorganized a bit

--- quinta-2-dez-tic-tac-toe-matrix/script.js
+++ quinta-2-dez-tic-tac-toe-matrix/script.js
@@ -3,24 +3,25 @@ let matrix = [
     [4, 5, 6],
     [7, 8, 9]
 ]
-
 let result = document.querySelector('#result')
-
 let turn = 'O'
 
 function areYouWinning() {
+    //Checando colunas
     for (let i = 0; i < 3; i++){
         if (matrix[i][0] == matrix[i][1] && matrix[i][1] == matrix[i][2]) {
             result.innerHTML = `${matrix[i][0]} VENCEU`
         }
     }
-
+    
+    //Checando linhas
     for (let j = 0; j < 3; j++){
         if (matrix[0][j] == matrix[1][j] && matrix[1][j] == matrix[2][j]) {
             result.innerHTML = `${matrix[0][j]} VENCEU`
         }
     }
-
+    
+    //Checando diagonais
     if (matrix[0][0] == matrix[1][1] && matrix[1][1] == matrix[2][2]) {
         result.innerHTML = `${matrix[0][0]} VENCEU`
     }
@@ -54,84 +55,24 @@ function changeTurn() {
         turn = 'X'
     }
 }
-function mark1() {
-    if (matrix[0][0] == 'O' || matrix[0][0] == 'X') {
+
+function markPosition(i, j) {
+    // IF para desconsiderar itens já marcados caso clique novamente
+    if (matrix[i][j] == 'O' || matrix[i][j] == 'X') {
         return
     }
-    matrix[0][0] = turn
-    document.querySelector('#p11').innerHTML = turn
+    matrix[i][j] = turn
+    document.querySelector(`#p${i}${j}`).innerHTML = turn
     changeTurn()
     areYouWinning()
 }
-function mark2() {
-    if (matrix[0][1] == 'O' || matrix[0][1] == 'X') {
-        return
-    }
-    matrix[0][1] = turn
-    document.querySelector('#p12').innerHTML = turn
-    changeTurn()
-    areYouWinning()
-}
-function mark3() {
-    if (matrix[0][2] == 'O' || matrix[0][2] == 'X') {
-        return
-    }
-    matrix[0][2] = turn
-    document.querySelector('#p13').innerHTML = turn
-    changeTurn()
-    areYouWinning()
-}
-function mark4() {
-    if (matrix[1][0] == 'O' || matrix[1][0] == 'X') {
-        return
-    }
-    matrix[1][0] = turn
-    document.querySelector('#p21').innerHTML = turn
-    changeTurn()
-    areYouWinning()
-}
-function mark5() {
-    if (matrix[1][1] == 'O' || matrix[1][1] == 'X') {
-        return
-    }
-    matrix[1][1] = turn
-    document.querySelector('#p22').innerHTML = turn
-    changeTurn()
-    areYouWinning()
-}
-function mark6() {
-    if (matrix[1][2] == 'O' || matrix[1][2] == 'X') {
-        return
-    }
-    matrix[1][2] = turn
-    document.querySelector('#p23').innerHTML = turn
-    changeTurn()
-    areYouWinning()
-}
-function mark7() {
-    if (matrix[2][0] == 'O' || matrix[2][0] == 'X') {
-        return
-    }
-    matrix[2][0] = turn
-    document.querySelector('#p31').innerHTML = turn
-    changeTurn()
-    areYouWinning()
-}
-function mark8() {
-    if (matrix[2][1] == 'O' || matrix[2][1] == 'X') {
-        return
-    }
-    matrix[2][1] = turn
-    document.querySelector('#p32').innerHTML = turn
-    changeTurn()
-    areYouWinning()
-}
-function mark9() {
-    if (matrix[2][2] == 'O' || matrix[2][2] == 'X') {
-        return
-    }
-    matrix[2][2] = turn
-    document.querySelector('#p33').innerHTML = turn
-    changeTurn()
-    areYouWinning()
-}
+
+function mark1() {markPosition(0, 0)}
+function mark2() {markPosition(0, 1)}
+function mark3() {markPosition(0, 2)}
+function mark4() {markPosition(1, 0)}
+function mark5() {markPosition(1, 1)}
+function mark6() {markPosition(1, 2)}
+function mark7() {markPosition(2, 0)}
+function mark8() {markPosition(2, 1)}
+function mark9() {markPosition(2, 2)}
