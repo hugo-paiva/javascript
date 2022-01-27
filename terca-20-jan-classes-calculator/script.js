af2b6fd0bc80d4f10bc5d441788e49862352c32a
hugo <hugosousa76@gmail.com>
2022-01-27 19:30:29 -0300

    refactor and numeric numbers added

--- terca-20-jan-classes-calculator/script.js
+++ terca-20-jan-classes-calculator/script.js
@@ -38,35 +38,90 @@ class Calculator {
 }
 
 let myCalc = new Calculator()
+// Initializing numeric buttons 
+const bttn1 = document.getElementById('bttn-1')
+const bttn2 = document.getElementById('bttn-2')
+const bttn3 = document.getElementById('bttn-3')
+const bttn4 = document.getElementById('bttn-4')
+const bttn5 = document.getElementById('bttn-5')
+const bttn6 = document.getElementById('bttn-6')
+const bttn7 = document.getElementById('bttn-7')
+const bttn8 = document.getElementById('bttn-8')
+const bttn9 = document.getElementById('bttn-9')
+const bttn0 = document.getElementById('bttn-0')
 
-const num1 = document.querySelector('#primeiro')
-const num2 = document.querySelector('#segundo')
-const sum = document.getElementById('sum')
-const minus = document.getElementById('minus')
-const times = document.getElementById('times')
-const divide = document.getElementById('divide')
-const equals = document.getElementById('equals')
-const clear = document.getElementById('clear-entry')
+// initializing operation buttons
+const sumButton = document.getElementById('sum')
+const minusButton = document.getElementById('minus')
+const timesButton = document.getElementById('times')
+const divideButton = document.getElementById('divide')
+const equalsButton = document.getElementById('equals')
+const clearButton = document.getElementById('clear-entry')
 const result = document.querySelector('p')
 
-num1.addEventListener('input', () => {
-    myCalc.setOperand1(Number(num1.value))
-})
+const visor = document.getElementById('visor')
 
-num2.addEventListener('input', () => {
-    myCalc.setOperand2(Number(num2.value))
-})
+let cacheVisor = ''
 
-sum.addEventListener('click', () =>  myCalc.setOperation('soma') )
-minus.addEventListener('click', () =>  myCalc.setOperation('subtracao') )
-times.addEventListener('click', () =>  myCalc.setOperation('multiplicacao') )
-divide.addEventListener('click', () =>  myCalc.setOperation('divisao') )
-equals.addEventListener('click', () => {
+function updateCacheAndVisor(_num) {
+    cacheVisor += _num
+    visor.innerHTML = cacheVisor
+}
+
+function saveOperand1FromVisor() {
+    myCalc.setOperand1(Number(cacheVisor))
+    cacheVisor = ''
+    visor.innerHTML = cacheVisor
+}
+
+bttn1.addEventListener('click', () => updateCacheAndVisor(1))
+
+bttn2.addEventListener('click', () => updateCacheAndVisor(2))
+
+bttn3.addEventListener('click', () => updateCacheAndVisor(3))
+
+bttn4.addEventListener('click', () => updateCacheAndVisor(4))
+
+bttn5.addEventListener('click', () => updateCacheAndVisor(5))
+
+bttn6.addEventListener('click', () => updateCacheAndVisor(6))
+
+bttn7.addEventListener('click', () => updateCacheAndVisor(7))
+
+bttn8.addEventListener('click', () => updateCacheAndVisor(8))
+
+bttn9.addEventListener('click', () => updateCacheAndVisor(9))
+
+bttn0.addEventListener('click', () => updateCacheAndVisor(0))
+
+sumButton.addEventListener('click', () =>  {
+    saveOperand1FromVisor()
+    myCalc.setOperation('soma')
+} )
+
+minusButton.addEventListener('click', () =>  {
+    saveOperand1FromVisor()
+    myCalc.setOperation('subtracao')
+} )
+
+timesButton.addEventListener('click', () =>  {
+    saveOperand1FromVisor()
+    myCalc.setOperation('multiplicacao')
+} )
+
+divideButton.addEventListener('click', () =>  {
+    saveOperand1FromVisor()
+    myCalc.setOperation('divisao')
+} )
+
+equalsButton.addEventListener('click', () => {
+    myCalc.setOperand2(Number(cacheVisor))
+    cacheVisor = ''
+    visor.innerHTML = cacheVisor
     result.innerHTML = myCalc.getResult()
 })
-clear.addEventListener('click', () =>  {
+
+clearButton.addEventListener('click', () =>  {
     result.innerHTML = 'RESULTADO'
     myCalc.clearCalculator()
-    num1.value = ''
-    num2.value = ''
 } )
